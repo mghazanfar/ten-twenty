f4164c104c5a1bb08ea7328d1c760f81cf41630f
Fahad <ghazanfar89@gmail.com>
2022-01-23 01:22:24 +0400

    Refactor

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Navbar } from "./components/navbar";
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <Navbar />
       </div>
     );
--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -3,57 +3,25 @@ import { day } from "../../theme.day";
 import { Divider } from "../divider";
 import { menu, night, today } from "../svg";
 import { Roboto } from "../typography";
+import { navbarStyles } from "./styles";
 
 const links = ["NEWS", "OPINION", "LIFE", "BUSINESS", "MAGAZINE", "NEWSLETTER"];
 
 export const Navbar = () => {
-  let theme = day;
   return (
     <>
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          fontFamily: "Roboto",
-          paddingTop: 25,
-          paddingLeft: 65,
-          paddingBottom: 48,
-        }}
-      >
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <Roboto style={{ fontWeight: 700 }}>{menu}</Roboto>
-          <Roboto style={{ marginLeft: 13, marginRight: 22, fontWeight: 700 }}>
-            MENU
-          </Roboto>
+      <div style={navbarStyles.header}>
+        <div style={navbarStyles.center}>
+          <div>{menu}</div>
+          <Roboto style={navbarStyles.menu}>MENU</Roboto>
           <div>{night}</div>
         </div>
-        <div
-          style={{
-            flex: 1,
-            textAlign: "center",
-            fontSize: 65,
-            marginLeft: "-12%",
-          }}
-        >
-          {today}
-        </div>
+        <div style={navbarStyles.title}>{today}</div>
       </div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          paddingBottom: 30,
-        }}
-      >
-        <div
-          style={{
-            width: "60%",
-            display: "flex",
-            justifyContent: "space-between",
-          }}
-        >
+      <div style={navbarStyles.subheader}>
+        <div style={navbarStyles.links}>
           {links.map((link) => (
-            <Roboto style={{ fontWeight: 700 }}>{link}</Roboto>
+            <Roboto style={navbarStyles.bold}>{link}</Roboto>
           ))}
         </div>
       </div>

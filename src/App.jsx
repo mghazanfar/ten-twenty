597c5eb1b2f183ba5d89d1a59cb87dd8c3f120b4
Fahad <ghazanfar89@gmail.com>
2022-01-23 01:46:11 +0400

    Create component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import { Navbar } from "./components/navbar";
+import { Main } from "./sections/main";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
+        <div style={{ margin: "41px 0px" }}>
+          <Main />
+        </div>
       </div>
     );
   }

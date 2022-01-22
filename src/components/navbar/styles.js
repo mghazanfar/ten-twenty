7a5fee1e2c8010f5b55cb0dbdda42f83dd1bc152
Fahad <ghazanfar89@gmail.com>
2022-01-23 01:22:28 +0400

    Refactor

---
+++ src/components/navbar/styles.js
@@ -0,0 +1,31 @@
+export const navbarStyles = {
+header:{
+    display: "flex",
+    alignItems: "center",
+    fontFamily: "Roboto",
+    paddingTop: 25,
+    paddingLeft: 65,
+    paddingBottom: 48,
+  },
+  center:{ display: "flex", alignItems: "center" },
+  menu:{ marginLeft: 13, marginRight: 22, fontWeight: 700 },
+  title:{
+    flex: 1,
+    textAlign: "center",
+    fontSize: 65,
+    marginLeft: "-12%",
+  },
+  subheader:{
+    display: "flex",
+    justifyContent: "center",
+    paddingBottom: 30,
+  },
+  links:{
+    width: "60%",
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  bold:{
+      fontWeight:700
+  }
+}

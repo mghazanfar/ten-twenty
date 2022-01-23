5711e63772bc59e25c40ef24fdd8f1c8c17cbddd
Fahad <ghazanfar89@gmail.com>
2022-01-23 15:57:56 +0400

    Themify section colors

---
+++ src/sections/horizontal/style.js
@@ -0,0 +1,11 @@
+export const useStyles = {
+    container:{
+        display: "flex",
+        justifyContent: "space-between",
+        marginTop: 50,
+      },
+      item:{
+          width:"30%"
+      },
+      header:{ marginTop: 40, marginBottom: 40 }
+}
---
+++ src/sections/perspective/styles.js
@@ -0,0 +1,15 @@
+export const useStyles = theme => {
+    return {
+        container: {
+            marginTop: 40,
+            marginBottom: 40,
+            backgroundColor: theme.peopleBg,
+            padding: "49px 110px",
+            display: "flex",
+            justifyContent: "space-between",
+          },
+          item:{
+              width:"32%"
+          }
+    }
+}
